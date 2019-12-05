3f8c1e471ca53b2fa635c362bf62a933613b8b6f
standa <ondra.stanicek@gmail.com>
2019-12-05 13:42:58 +0100

    added .gitconfig, updated symlink.sh accordingly (and added forgotten backup)

--- symlink.sh
+++ symlink.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 mv ~/.vimrc ~/.vimrc.backup
+mv ~/.bashrc ~/.bashrc.backup
+mv ~/.gitconfig ~/.gitconfig.backup
 mv ~/.config/nvim/init.vim ~/.config/nvim/init.vim.backup
 
 path=$(cd `dirname $0` && pwd) # gets absolute path to the directory this script is contained in
 
 ln -s "$path"/.vimrc ~/.vimrc
+ln -s "$path"/.gitconfig ~/.gitconfig
 ln -s "$path"/init.vim ~/.config/nvim/init.vim
 
 cat "$path"/custom_bashrc >> ~/.bashrc
