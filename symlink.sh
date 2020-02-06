214409c54d01fceb3618863d625efa937d6922fa
standa <ondra.stanicek@gmail.com>
2020-02-06 12:09:19 +0000

    install vim-plug automatically

--- symlink.sh
+++ symlink.sh
@@ -11,3 +11,6 @@ ln -s "$path"/.gitconfig ~/.gitconfig
 ln -s "$path"/init.vim ~/.config/nvim/init.vim
 
 cat "$path"/custom_bashrc >> ~/.bashrc
+
+# install vim-plug
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
